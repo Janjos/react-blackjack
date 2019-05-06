73f425c21f7123ab748a2b79ec771eda2d8178bc
Jonathan <jdamacena.anjos@gmail.com>
2019-05-05 22:58:35 -0300

    adding fonts and put globalStyle styles out from App

--- src/App.js
+++ src/App.js
@@ -3,35 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import reducers from './redux/reducers'
-import { createGlobalStyle } from 'styled-components'
 
 import { GameScreen } from './screens/gameScreen/GameScreen'
 import { initialState } from './constants/data/initialState'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './redux/sagas'
-
-const cursorImage = require('./assets/images/interface/white-cursor.png')
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    cursor: url(${cursorImage}), default;
-  }
-
-  body {
-    margin: 0;
-    padding: 0;
-  }
-
-  img {
-    image-rendering: pixelated;
-    image-rendering: -moz-crisp-edges;
-    image-rendering: crisp-edges;
-  }
-
-  div {
-    box-sizing: border-box;
-  }
-`
+import { GlobalStyle } from './sharedComponents/globalStyle'
 
 export default function App () {
   const sagaMiddleWare = createSagaMiddleware()
---
+++ src/sharedComponents/globalStyle.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+const cursorImage = require('../assets/images/interface/white-cursor.png')
+const pixelatedFont = require('../assets/fonts/Pixeled.woff')
+const pixelatedFont2 = require('../assets/fonts/Pixeled.woff2')
+const fippsFont = require('../assets/fonts/Fipps-Regular.woff2')
+const fippsFont2 = require('../assets/fonts/Fipps-Regular.woff')
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    cursor: url(${cursorImage}), default;
+    font-family: 'Pixeled'
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    filter: blur(.2px)
+  }
+
+  img {
+    image-rendering: pixelated;
+    image-rendering: -moz-crisp-edges;
+    image-rendering: crisp-edges;
+  }
+
+  div {
+    box-sizing: border-box;
+  }
+
+  @font-face {
+    font-family: 'Pixeled';
+    src: url(${pixelatedFont2}) format('woff2'),
+        url(${pixelatedFont}) format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'Fipps';
+    src: url(${fippsFont2}) format('woff2'),
+        url(${fippsFont}) format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+`
