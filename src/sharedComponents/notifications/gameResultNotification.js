4bb584e48bdcc9759bbe9d56dec1b26c223ea875
Jonathan <jdamacena.anjos@gmail.com>
2019-05-06 23:15:13 -0300

    Removing useless import from gameResultNotification

--- src/sharedComponents/notifications/gameResultNotification.js
+++ src/sharedComponents/notifications/gameResultNotification.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { GameResult } from '../../classes/GameResult'
 import styled from 'styled-components'
