8aff705d42f1d78b478fa7a6525a16b2349a73b8
Jonathan <jdamacena.anjos@gmail.com>
2019-05-08 03:35:35 -0300

    Adding firebase link to readme

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -112,6 +112,7 @@ function * resetGame () {
 
   yield delay(endGameDelay * 2)
   yield put(resetCards())
+  yield delay(cardAnimationTime)
   yield put(resetGameResult())
 
   yield put(makeBet(0))
